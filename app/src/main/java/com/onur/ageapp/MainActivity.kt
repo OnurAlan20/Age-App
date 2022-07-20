e023b03e75d5ccf8255608918135c313bfdd95a1
Onur <onuralann20@gmail.com>
2022-07-20 16:29:17 +0300

    third

--- app/src/main/java/com/onur/ageapp/MainActivity.kt
+++ app/src/main/java/com/onur/ageapp/MainActivity.kt
@@ -10,13 +10,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnClickMe = findViewById<Button>(R.id.btnclick1)
-        btnClickMe.setOnClickListener {
-
-        }
         val textView1 = findViewById<TextView>(R.id.textview1)
-        textView1.setOnClickListener {
+        val btnClickMe = findViewById<Button>(R.id.btnclick1)
+        val textCounter = findViewById<TextView>(R.id.counter)
+        var countValue = 0
+
+
+        btnClickMe.setOnClickListener {
             textView1.text = " You Click Me Bro!"
+            textView1.text = " You Click Me Bro!231"
+            countValue+=1
+            textCounter.text = countValue.toString()
         }
+
+
+
     }
 }
