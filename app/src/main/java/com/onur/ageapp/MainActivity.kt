95dab7882da0f40584b62bd2ede72d5be72d8ef3
OnurAlan20 <83537043+OnurAlan20@users.noreply.github.com>
2022-07-28 16:33:39 +0300

    Final step of app

--- app/src/main/java/com/onur/ageapp/MainActivity.kt
+++ app/src/main/java/com/onur/ageapp/MainActivity.kt
@@ -3,51 +3,88 @@ package com.onur.ageapp
 import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.DatePicker
 import android.widget.TextView
-import android.widget.Toast
-import java.time.DayOfWeek
-import java.time.Month
-import java.time.Year
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var formatedMonth: String? = null
+    private var formatedDay: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val btnDatePicker = findViewById<Button>(R.id.btn_date_picker)
 
-        //when you click button the code which in the setOnClickListener will work.
+
         btnDatePicker.setOnClickListener {
+
             clickDatePicker()
 
         }
 
-
-
     }
+    private fun clickDatePicker(){
+        val myCalender = Calendar.getInstance()
+        val year = myCalender.get(Calendar.YEAR)
+        val month = myCalender.get(Calendar.MONTH)
+        val day = myCalender.get(Calendar.DAY_OF_MONTH)
+        val textViewCounter = findViewById<TextView>(R.id.textViewCounter)
 
-    //This is How Can you do a date picker dialog.
-    fun clickDatePicker(){
+        val textViewSelectedDate = findViewById<TextView>(R.id.textViewSelectedDate)
 
-        val myCalendar = Calendar.getInstance()
-        val year = myCalendar.get(Calendar.YEAR)
-        val month = myCalendar.get(Calendar.MONTH)
-        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+        val dpd = DatePickerDialog(
+            this,
+            { _, selectedYear , selectedMonth, selectedDay ->
 
-        DatePickerDialog(this,
-            DatePickerDialog.OnDateSetListener{
-                view, year, month, dayOfMonth ->
-                Toast.makeText(this,"Date Picker Dialog works well!",Toast.LENGTH_LONG).show()
+                formattingDate(selectedMonth,selectedDay)
+                val selectedDate = "$formatedDay/$formatedMonth/$selectedYear"
+                textViewSelectedDate.text = selectedDate
+
+                val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+                val theDate = sdf.parse(selectedDate)
+                val selectedDayInMinutes = theDate.time /60000
+
+                val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+                val currentDateInMınutes = currentDate.time/60000
+
+                val diffirenceInMinutes= currentDateInMınutes - selectedDayInMinutes
+
+                textViewCounter.text = diffirenceInMinutes.toString()
+
             },
             year,
             month,
             day
-
-        ).show()
-
+        )
+        dpd.datePicker.maxDate = System.currentTimeMillis()
+        dpd.show()
     }
+
+    private fun formattingDate(selectedMonth: Int,selectedDay: Int){
+
+        if (selectedMonth.toString().length < 2){
+            formatedMonth = "0${selectedMonth+1}"
+        }
+        else{
+            formatedMonth =(selectedMonth+1).toString()
+        }
+        if (selectedDay.toString().length < 2){
+            formatedDay = "0$selectedDay"
+        }
+        else{
+            formatedDay =selectedDay.toString()
+        }
+    }
+
+
+
+
+
+
+
+
 }
